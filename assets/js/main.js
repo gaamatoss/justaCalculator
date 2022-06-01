b6891da6ea14dee3a57f59bfdbb8b267551c84f4
Gabriel Matos <gsilva505@hotmail.com>
2022-05-31 21:59:49 -0300

    arrumando detalhe do front e comecando a logica das operacoes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,54 @@
-let listBtnVal = document.querySelectorAll(".btn__value")
+let btnValue = document.querySelectorAll(".btn__value")
+let btnOperation = document.querySelectorAll(".btn__simbol")
 let visor = document.querySelector(".input__val")
 let cleanVisor = document.querySelector(".btnDel")
+let equal = document.querySelector('.equal')
+
 
 cleanVisor.addEventListener("click", function () {
     visor.value = ""
 })
 
-for (let i = 0; i < listBtnVal.length; i++) {
-    const tecla = listBtnVal[i]
+equal.addEventListener("click", function () {
+    console.log(visor.value);
+    operationCalc(operationType);
+})
+
+for (let i = 0; i < btnValue.length; i++) {
+    const tecla = btnValue[i]
     tecla.onclick = (e) => {
         valType = e.target.textContent;
         visor.value = `${visor.value}` + `${valType}`;
     }
 }
 
-function plus() {
-
+for (let i = 0; i < btnOperation.length; i++) {
+    const tecla = btnOperation[i]
+    tecla.onclick = (e) => {
+        operationType = e.target.textContent;
+        if (operationType == '+') {
+            operationCalc(operationType);
+        } else if (operationType == '-') {
+            operationCalc(operationType);
+        } else if (operationType == '*') {
+            operationCalc(operationType);
+        } else {
+            operationCalc(operationType);
+        }
+    }
 }
 
-function minus() {
 
-}
+function operationCalc(operationType) {
+    // let operationVal = []
+    // let firstOp = visor.value + operationType
+    // console.log("1: " + firstOp);
+    // visor.value = ""
 
-function multiply() {
+    // let secondOp = visor.value
+    // console.log("2: " + secondOp);
+    // let soma = a + b
+    // console.log(soma);
 
-}
-
-function divider() {
 
 }
